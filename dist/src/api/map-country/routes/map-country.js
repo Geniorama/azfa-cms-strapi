93b5670d5d0c9765ee79115ccd4b77ec0f1f0e4c
angelpublicista <angelpublicista.1@gmail.com>
2025-08-20 17:37:01 -0500

    fix: modify package json

--- dist/src/api/map-country/routes/map-country.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-/**
- * map-country router
- */
-Object.defineProperty(exports, "__esModule", { value: true });
-const strapi_1 = require("@strapi/strapi");
-exports.default = strapi_1.factories.createCoreRouter('api::map-country.map-country');
--- dist/src/api/real-state-offer/services/real-state-offer.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-/**
- * real-state-offer service
- */
-Object.defineProperty(exports, "__esModule", { value: true });
-const strapi_1 = require("@strapi/strapi");
-exports.default = strapi_1.factories.createCoreService('api::real-state-offer.real-state-offer');
