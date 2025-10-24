1da91567579142a8678f38205d68233a2977db34
angelpublicista <angelpublicista.1@gmail.com>
2025-10-24 07:12:36 -0500

    feat: is editor in user fields

--- types/generated/contentTypes.d.ts
+++ types/generated/contentTypes.d.ts
@@ -2840,6 +2840,7 @@ export interface PluginUsersPermissionsUser
       Schema.Attribute.SetMinMaxLength<{
         minLength: 6;
       }>;
+    isEditor: Schema.Attribute.Boolean & Schema.Attribute.DefaultTo<false>;
     locale: Schema.Attribute.String & Schema.Attribute.Private;
     localizations: Schema.Attribute.Relation<
       'oneToMany',
